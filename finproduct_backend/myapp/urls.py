45d8733d2a727ddae9d7ef1678b678543fd4ee82
Dileepnaik14 <dileepnaik1444@gmail.com>
2024-10-14 15:57:06 +0530

    commited changes

--- finproduct_backend/myapp/urls.py
+++ finproduct_backend/myapp/urls.py
@@ -9,7 +9,7 @@ from .views import CreateProductView, ProductListView, ParameterListView, Create
 urlpatterns = [
     path('accounts/create/', create_account, name='create-account'),
     path('accounts/<int:product_id>/', get_accounts_by_product, name='get-accounts-by-product'),
-     path('accounts/adminid/<int:admin_id>/', get_accounts_by_adminid, name='get-accounts-by-product'),
+    path('accounts/adminid/<int:admin_id>/', get_accounts_by_adminid, name='get-accounts-by-product'),
     path('product/account/<int:account_id>/', get_product_by_account, name='get_product_by_account'),
     path('api/products/', CreateProductView.as_view(), name='create-product'),
     path('api/products/list/', ProductListView.as_view(), name='product-list'),
